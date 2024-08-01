f04748a031dee2ff709d831091b3ca629ac41f9b
limon <pentagonocuadrado2@gmail.com>
2024-07-31 19:35:47 -0500

    funcion suma numeros2

--- SumaNumeros/src/main/java/com/mycompany/sumanumeros/SumaNumeros.java
+++ SumaNumeros/src/main/java/com/mycompany/sumanumeros/SumaNumeros.java
@@ -7,17 +7,18 @@ public class SumaNumeros {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
-
+        System.out.println("digite un numero");
         int num = entrada.nextInt();
+        System.out.println("digite un segundo numero");
         int num2 = entrada.nextInt();
 
-        sumaNumeros(num,num2 );
+        sumaNumeros(num,num2);
 
     }
 
-    public static int sumaNumeros(int num,int num2) {
+    public static void sumaNumeros(int num,int num2) {
 
         int suma = num + num2;
-        return suma;
+        System.out.println(suma);
     }
 }
